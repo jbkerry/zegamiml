0f383de41152c7daa8c6ede99e3c0252d393ee33
Jon Kerry <jon.kerry@imm.ox.ac.uk>
2017-05-09 14:28:52 +0100

    zegamiml now accepts matrix format file

--- zegamiml.py
+++ zegamiml.py
@@ -7,8 +7,9 @@ import argparse
 parser = argparse.ArgumentParser(description="This is a script that applies unsupervised learning to a directory of images and appends x and y params to be used by scatterplot in Zegami.");
 parser.add_argument('-i','--input', help='Input file name (should be a TSV)',required=True)
 parser.add_argument('-o','--output', help='Output file name (will be original TSV with the X and Y columns appended',required=True)
-parser.add_argument('-c','--image_column', help='Column name in TSV that contains the image name',required=True)
-parser.add_argument('-d','--image_dir', help='Directory that contains all the images.',required=True)
+parser.add_argument('-f','--format', help="Input file format. Image or Matrix", required=False, default="Image")
+parser.add_argument('-c','--image_column', help='Column name in TSV that contains the image name',required=False)
+parser.add_argument('-d','--image_dir', help='Directory that contains all the images.',required=False)
 parser.add_argument('-a','--analysis_type', help='PCA or TSNE.',required=False, default="PCA")
 parser.add_argument('-p','--plot', help='Do a plot based on resulting X and Y coords (requires PIL).',action='store_true', required=False)
 args = parser.parse_args()
@@ -18,7 +19,7 @@ import numpy as np
 import pandas as pd
 import pylab as pl
 import sys
-from sklearn.decomposition import RandomizedPCA
+from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
@@ -37,12 +38,12 @@ def img_to_matrix(filename, verbose=False):
     takes a filename and turns it into a numpy array of RGB pixels
     """
     img = Image.open(filename)
-    print "Opening "+filename
+    print("Opening "+filename)
     if verbose==True:
-        print "changing size from %s to %s" % (str(img.size), str(STANDARD_SIZE))
+        print("changing size from %s to %s" % (str(img.size), str(STANDARD_SIZE)))
     # use PIL to resize to above   
     img = img.resize(STANDARD_SIZE)
-    print str(img.getdata())
+    print(str(img.getdata()))
     # Returns the contents of this image as a sequence object containing pixel values.
     img = list(img.getdata())
     img = map(list, img)
@@ -61,22 +62,29 @@ def flatten_image(img):
     return img_wide[0]
 
 # read in the zegami table
-zt = pd.read_csv(args.input,sep="\t", header= 0)
 
-# get a list of images via path
-images = zt[args.image_column]
+if args.format=="Matrix":
+    zt = pd.read_csv(args.input,sep='\t',skiprows=1,header=None)
+    zt_df = zt.loc[:,6:]
+    zt_df.colums = range(100)
+    zt_df = zt_df.fillna(0)
+    data = zt_df.values
+else:
+    zt = pd.read_csv(args.input,sep="\t", header= 0)
+    # get a list of images via path
+    images = zt[args.image_column]
+    data = []
+    labels = []
+    print("Processing a "+str(images))
+    for image in images:
+        image = args.image_dir+"/"+image
+        img = img_to_matrix(image)
+        img = flatten_image(img)
+        data.append(img)
 
-data = []
-labels = []
 
-print "Processing a "+str(images)
-for image in images:
-	image = args.image_dir + "/" + image
-	img = img_to_matrix(image)
-	img = flatten_image(img)
-	data.append(img)
 
-print "Creating PCA or TSNE..."
+print("Creating PCA or TSNE...")
 # unhash the one you want TSNE is more sensitive 
 # but plots will be different each time
 
@@ -85,7 +93,7 @@ if (args.analysis_type == "TSNE"):
 	#pca = TSNE(n_components=2, init='pca', random_state=0)
 else:
 #or can do PCA
-	pca = RandomizedPCA(n_components=2)
+	pca = PCA(svd_solver='randomized',n_components=2)
 
 X = pca.fit_transform(data)
 df = pd.DataFrame({"x": X[:, 0], "y": X[:, 1]})
@@ -94,11 +102,11 @@ y_label =  args.analysis_type + "_y"
 zt[x_label]=df['x']
 zt[y_label]=df['y']
 
-print "=========================="
+print("==========================")
 #print str(df)
-print "Appended x and y coordinates to "+args.input+" and created "+args.output+"."
+print("Appended x and y coordinates to "+args.input+" and created "+args.output+".")
 zt.to_csv(args.output,sep="\t",index=False)
-print "=========================="
+print("==========================")
 
 
 # Do a scatter plot, you can leave this out
