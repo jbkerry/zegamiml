d2851f422a9cff96c0da515b3310ad210302405f
Jon Kerry <jon.kerry@imm.ox.ac.uk>
2017-06-01 10:46:28 +0100

    started merge_groups.py

---
+++ merge_groups.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import pandas as pd
+
+def Reset():
+    '''Starts as new data-frame or can be used to reset
+    a filled data-frame
+    
+    Returns
+    -------
+    new_df: empty data-frame with Zegami headers as columns
+    
+    '''
+    
+    new_df = pd.DataFrame(columns = ['feature_id', 'image', 'chr',
+                'bp start', 'bp end', 'FDR',
+                'fold_enrichment', 'negLog10Pvalue',
+                'num_tags', 'peak_length', 'summit',
+                'TSNE_JK_x', 'TSNE_JK_y', 'Tags', 'plot_name', 'peak_type'])
+    
+    return new_df
+
+def AppendGroup(df, table, name, peak_type):
+    '''Append and import sub data-frame to the main data-frame
+    
+    Parameters
+    ----------
+    df: the pandas data-frame to append new rows to
+    table: tab-delimited output from zegami
+    name: name of this plot
+    peak_type: are the peaks low, high, absent etc.
+    
+    Returns
+    -------
+    update_df: updated df consisting of original plus the new data-frame
+    
+    '''
+    
+    _temp_df = pd.read_csv(table, sep='\t', header=0)
+    _temp_df['plot_name'] = name
+    _temp_df['peak_type'] = peak_type
+    update_df = df.append(_temp_df, ignore_index=True)
+    
+    return update_df
