bd78d18531cd9876adc9321829fda5b10bc5b039
Jon Kerry <jon.kerry@imm.ox.ac.uk>
2017-06-01 11:01:01 +0100

    append_group function now takes 'position' kwarg

--- merge_groups.py
+++ merge_groups.py
@@ -2,7 +2,7 @@
 
 import pandas as pd
 
-def Reset():
+def reset():
     '''Starts as new data-frame or can be used to reset
     a filled data-frame
     
@@ -16,11 +16,12 @@ def Reset():
                 'bp start', 'bp end', 'FDR',
                 'fold_enrichment', 'negLog10Pvalue',
                 'num_tags', 'peak_length', 'summit',
-                'TSNE_JK_x', 'TSNE_JK_y', 'Tags', 'plot_name', 'peak_type'])
+                'TSNE_JK_x', 'TSNE_JK_y', 'Tags', 'plot_name',
+                'peak_type', 'position'])
     
     return new_df
 
-def AppendGroup(df, table, name, peak_type):
+def append_group(df, table, name, peak_type, position):
     '''Append and import sub data-frame to the main data-frame
     
     Parameters
@@ -29,6 +30,7 @@ def AppendGroup(df, table, name, peak_type):
     table: tab-delimited output from zegami
     name: name of this plot
     peak_type: are the peaks low, high, absent etc.
+    position: location on TSNE plot
     
     Returns
     -------
@@ -39,6 +41,7 @@ def AppendGroup(df, table, name, peak_type):
     _temp_df = pd.read_csv(table, sep='\t', header=0)
     _temp_df['plot_name'] = name
     _temp_df['peak_type'] = peak_type
+    _temp_df['position'] = position
     update_df = df.append(_temp_df, ignore_index=True)
     
     return update_df
