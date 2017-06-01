87a227e4dae90138d114176d159499438fa88ef3
Jon Kerry <jon.kerry@imm.ox.ac.uk>
2017-06-01 13:26:54 +0100

    added merge_plot_data function

--- zegami_tools.py
+++ zegami_tools.py
@@ -46,3 +46,41 @@ def append_group(df, table, name, peak_type, position):
     update_df = df.append(_temp_df, ignore_index=True)
     
     return update_df
+
+def merge_plot_data(df, plot_file, dd=True):
+    '''Merges peaks in a zegami output dataframe with
+    plot data from a text file with bigWig values in
+    100 bins across the peak
+    
+    Parameters
+    ----------
+    df: the zegami info data-frames with the required
+        peak IDs
+    plot_file: a tab-delimited file with values for
+        all peaks, split into 100 bins
+    dd: boolean, drop duplicates in the merged data-frame,
+        default = True
+        
+    Returns
+    -------
+    data: a 2-dimensional numpy array containing the
+        values for each peak from the zegami data-frame
+        for use in TSNE
+    '''
+    
+    _data_df = pd.read_csv(plot_file,sep='\t',skiprows=1,header=None)
+    _data_df = _data_df.fillna(0)
+    _data_df.insert(0, 'feature_id', _data_df[0]+"_"+_data_df[1].map(str)+"_"+_data_df[2].map(str))
+    _data_df = _data_df.drop_duplicates(subset='feature_id', keep='first')
+    
+    #_zegami_mat = pd.read_csv('PeakFeatures.tab',sep="\t")
+    _zegami_df = df
+    
+    _merge_df = pd.merge(_zegami_df, _data_df, on='feature_id')
+    if dd:
+        _merge_df = _merge_df.drop_duplicates(subset='feature_id',
+                                              keep='first')
+    
+    data = _merge_df.iloc[:,23:].values
+    
+    return data
